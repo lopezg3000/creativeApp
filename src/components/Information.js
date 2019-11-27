8f86aa3d34ae2fba740524542a5dd06f8d1c2439
Gabriel Lopez <lopezg3000@gmail.com>
2019-11-27 08:42:37 -0600

    importedPicInReactJS

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Recipe from "../Img/recipe.jpg"
 
 const Heading = () => {
     return (
@@ -8,7 +9,8 @@ const Heading = () => {
             <main>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eros arcu, aliquet vel efficitur non, feugiat mollis eros. Donec mi magna, eleifend vel auctor ac, rhoncus vel purus. Vivamus maximus libero at sem pharetra facilisis. Sed aliquam, risus et pulvinar rhoncus, dui mi vestibulum nulla, at tempor ante ante non augue. Quisque id tempus risus. Aenean mattis purus at imperdiet dictum. Nullam lacinia dictum risus, suscipit scelerisque lorem dictum ut. Ut vitae tortor odio. Curabitur pretium lacinia risus, eget dictum risus porta ac. Nam pretium lobortis felis. Mauris euismod magna nec purus ullamcorper dignissim.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eros arcu, aliquet vel efficitur non, feugiat mollis eros. Donec mi magna, eleifend vel auctor ac, rhoncus vel purus. Vivamus maximus libero at sem pharetra facilisis. Sed aliquam, risus et pulvinar rhoncus, dui mi vestibulum nulla, at tempor ante ante non augue. Quisque id tempus risus. Aenean mattis purus at imperdiet dictum. Nullam lacinia dictum risus, suscipit scelerisque lorem dictum ut. Ut vitae tortor odio. Curabitur pretium lacinia risus, eget dictum risus porta ac. Nam pretium lobortis felis. Mauris euismod magna nec purus ullamcorper dignissim.</p>
-            </main>
+            </main> {/*descriptive html tag. Helps with SEO and accessibility*/}
+            <img src={Recipe} />
         </div>
     );
 };
