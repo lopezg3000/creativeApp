b5f827a83ad755af6ef6b6cc00ad5a28d411ece2
Gabriel Lopez <lopezg3000@gmail.com>
2019-12-04 20:58:54 -0600

    deadLinkAddedButWithReactProblem

--- src/components/Information.js
+++ src/components/Information.js
@@ -4,7 +4,7 @@ import Recipe from "../Img/recipe.jpg"
 const Heading = () => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1><a target="_blank" href="#" rel="noopener noreferrer">Hello World</a></h1>
             <h2>Healthy Food Recipes App</h2> {/*Looking for new title*/}
             <main>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eros arcu, aliquet vel efficitur non, feugiat mollis eros. Donec mi magna, eleifend vel auctor ac, rhoncus vel purus. Vivamus maximus libero at sem pharetra facilisis. Sed aliquam, risus et pulvinar rhoncus, dui mi vestibulum nulla, at tempor ante ante non augue. Quisque id tempus risus. Aenean mattis purus at imperdiet dictum. Nullam lacinia dictum risus, suscipit scelerisque lorem dictum ut. Ut vitae tortor odio. Curabitur pretium lacinia risus, eget dictum risus porta ac. Nam pretium lobortis felis. Mauris euismod magna nec purus ullamcorper dignissim.</p>
