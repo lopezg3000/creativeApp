3175a0e17101d1f06d199e6b6f4971987f8696fa
Gabriel Lopez <lopezg3000@gmail.com>
2019-12-08 06:38:39 -0600

    imageLinkCreated

--- src/components/Information.js
+++ src/components/Information.js
@@ -11,7 +11,7 @@ const Heading = () => {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eros arcu, aliquet vel efficitur non, feugiat mollis eros. Donec mi magna, eleifend vel auctor ac, rhoncus vel purus. Vivamus maximus libero at sem pharetra facilisis. Sed aliquam, risus et pulvinar rhoncus, dui mi vestibulum nulla, at tempor ante ante non augue. Quisque id tempus risus. Aenean mattis purus at imperdiet dictum. Nullam lacinia dictum risus, suscipit scelerisque lorem dictum ut. Ut vitae tortor odio. Curabitur pretium lacinia risus, eget dictum risus porta ac. Nam pretium lobortis felis. Mauris euismod magna nec purus ullamcorper dignissim.</p>
             </main> {/*descriptive html tag. Helps with SEO and accessibility*/}
             <a href="https://www.delish.com/cooking/recipe-ideas/g3733/healthy-dinner-recipes/" target="_blank" rel="noopener noreferrer">Healthy Recipes</a> {/*Added target to open link in new*/}
-            <img style={{ height: "200vh", width: "175vh" }} src={Recipe} alt="Pasta with Vegetables" /> {/*importing images is different in react*/} {/*Alt tag used for screen readers to improve accessibility and is displayed if the image fails to load. If the image is purely decorative then using an empty alt attribute is best practice */}
+            <a href="https://www.lizshealthytable.com/2017/05/03/2-modern-italian-cooking-with-alexandra-caspero-rd/" target="_blank" rel="noopener noreferrer"><img style={{ height: "200vh", width: "175vh" }} src={Recipe} alt="Pasta with Vegetables" /> </a> {/*importing images is different in react*/ /*Alt tag used for screen readers to improve accessibility and is displayed if the image fails to load. If the image is purely decorative then using an empty alt attribute is best practice */}
         </div>
     );
 };
@@ -32,3 +32,14 @@ Give the anchor element a href attribute set to a value of string with url web a
 Give anchor element a rel attribute set to a value of stirng noopener noreferrer.
 */
 
+/*Make Dead Links Using the Hash Symbol
+
+Give anchor element href attribute and set the value to string #
+Do this before you know where the anchor element will link
+You can also use deadlinks when changing the behavior of the link with javascript
+*/
+
+
+/*Turn An Image Into a Link
+
+You can make elements into links by nesting them within anchor elements */
