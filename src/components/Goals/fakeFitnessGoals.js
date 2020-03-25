6e5cff9e51d9c0b3e24ee7b98fbb0df1b61358df
Gabriel Lopez <lopezg3000@gmail.com>
2020-03-24 21:28:23 -0500

    decreaseBodyFatObjAdded

--- src/components/Goals/fakeFitnessGoals.js
+++ src/components/Goals/fakeFitnessGoals.js
@@ -2,29 +2,57 @@
 
 const fitnessGoals = [
     {
-        _id: "5b21ca3eeb7f6fbccd471815",
-        title: "Build More Lean Muscle",
+        _id: "5b21ca3eeb7f6fbccd471816",
+        title: "Decrease Body Fat",
         description: (
             <p>
-                "With any diet, the most important thing to remember is to stay hydrated.
+                With any diet, the most important thing to remember is to stay hydrated.
                 Drinking water will support your body with digestion, transporting nutrition
-                to your body and even help improve cognitive function."
+                to your body and even help improve cognitive function.
             </p>
         )
     },
+
 
     {
         _id: "5b21ca3eeb7f6fbccd471816",
         title: "Decrease Body Fat",
         description: (
-            <p>
-                "With any diet, the most important thing to remember is to stay hydrated.
-                Drinking water will support your body with digestion, transporting nutrition
-                to your body and even help improve cognitive function."
-            </p>
+            <React.Fragment>
+                <p>
+                    This may seem like an obvious one but it is one of the most important
+                    steps to a healthier you. Reducing your body fat has a lot of benefits
+                    such as:
+                </p>
+                <ul>
+                    <li>Improved joints and tendons</li>
+                    <li>Lowered risk of diabetes</li>
+                    <li>Reduced risk of heart disease</li>
+                    <li>Reduced inflammation</li>
+                    <li>Better performance and endurance</li>
+                    <li>Improved appearance and confidence</li>
+                    <li></li>
+
+
+                    Better hormonal profiles in your body
+
+                </ul>
+
+            </React.Fragment>
         )
     },
 
+    {
+        _id: "5b21ca3eeb7f6fbccd471817",
+        title: "Drink More Water",
+        description: (
+            <p>
+                With any diet, the most important thing to remember is to stay hydrated.
+                Drinking water will support your body with digestion, transporting nutrition
+                to your body and even help improve cognitive function.
+            </p>
+        )
+    },
 
 
 ]
