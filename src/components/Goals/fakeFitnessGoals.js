7413a98d78be5a5cc2b90be3ce92eb80d4d3867a
Gabriel Lopez <lopezg3000@gmail.com>
2020-03-24 21:49:06 -0500

    EatMoreGreensObjAdded

--- src/components/Goals/fakeFitnessGoals.js
+++ src/components/Goals/fakeFitnessGoals.js
@@ -2,13 +2,12 @@
 
 const fitnessGoals = [
     {
-        _id: "5b21ca3eeb7f6fbccd471816",
-        title: "Decrease Body Fat",
+        _id: "5b21ca3eeb7f6fbccd471815",
+        title: "Build More Lean Muscle",
         description: (
             <p>
-                With any diet, the most important thing to remember is to stay hydrated.
-                Drinking water will support your body with digestion, transporting nutrition
-                to your body and even help improve cognitive function.
+                We all want to have lean muscles because not only does it look good,
+                it has great health benefits:
             </p>
         )
     },
@@ -18,27 +17,11 @@ const fitnessGoals = [
         _id: "5b21ca3eeb7f6fbccd471816",
         title: "Decrease Body Fat",
         description: (
-            <React.Fragment>
-                <p>
-                    This may seem like an obvious one but it is one of the most important
-                    steps to a healthier you. Reducing your body fat has a lot of benefits
-                    such as:
-                </p>
-                <ul>
-                    <li>Improved joints and tendons</li>
-                    <li>Lowered risk of diabetes</li>
-                    <li>Reduced risk of heart disease</li>
-                    <li>Reduced inflammation</li>
-                    <li>Better performance and endurance</li>
-                    <li>Improved appearance and confidence</li>
-                    <li></li>
-
-
-                    Better hormonal profiles in your body
-
-                </ul>
-
-            </React.Fragment>
+            <p>
+                This may seem like an obvious one but it is one of the most important
+                steps to a healthier you. Reducing your body fat has a lot of benefits
+                such as:
+            </p>
         )
     },
 
@@ -54,5 +37,49 @@ const fitnessGoals = [
         )
     },
 
+    {
+        _id: "5b21ca3eeb7f6fbccd471818",
+        title: "Eat More Greens",
+        description: (
+            <p>
+                What you eat is the most important factor in a healthier living plan.
+                It is important to ensure you are getting as many nutrients and vitamins
+                from the food you are eating as possible.<br />
+
+                Focus on dark, leafy greens as they will provide you with a wide array
+                of vitamins, minerals, nutrients and antioxidants – all of which you body
+                needs!
+            </p>
+        )
+    },
+
 
 ]
+
+
+{/* Will need to find a way to map it. Will probably have to use an array to map the benefits
+    need to make a column array like in the movie component.
+    might put it under prop additional info<ul>
+<li>Improved joints and tendons</li>
+<li>Lowered risk of diabetes</li>
+<li>Reduced risk of heart disease</li>
+<li>Reduced inflammation</li>
+<li>Better performance and endurance</li>
+<li>Improved appearance and confidence</li>
+<li>Better hormonal profiles in your body</li>
+
+
+
+
+</ul> */}
+
+
+// Will need to find a way to map it. Will probably have to use an array to map the benefits
+//     need to make a column array like in the movie component.
+//     might put it under prop additional info<ul>
+// Improved posture
+// Reduced body fat
+// Improved metabolism
+// Strong bones
+// Protects and improves joint health
+// Improves stamina
