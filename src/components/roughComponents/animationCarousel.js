3c9095f80dd6203b58e4c19e5a30afef0ece00c3
Gabriel Lopez <lopezg3000@gmail.com>
2020-03-28 23:02:25 -0500

    carouselCSSNeedToConvertToReact

---
+++ src/components/roughComponents/animationCarousel.js
@@ -0,0 +1,39 @@
+{/* <div class="scene">
+  <div class="carousel">
+    <div class="carousel__cell">1</div>
+    <div class="carousel__cell">2</div>
+    <div class="carousel__cell">3</div>
+    <div class="carousel__cell">4</div>
+    
+
+  </div>
+</div>
+<p style="text-align: center;">
+  <button class="previous-button">Previous</button>
+  <button class="next-button">Next</button>
+</p> */}
+
+//css
+// 
+
+
+// var carousel = document.querySelector('.carousel__cell:nth-child(1)');
+// var cellCount = 4;
+// var selectedIndex = 0;
+
+// function rotateCarousel() {
+//   var angle = selectedIndex / cellCount * -360;
+//   carousel.style.transform = 'translateZ(105px) rotateY(' + angle + 'deg)';
+// }
+
+// var prevButton = document.querySelector('.previous-button');
+// prevButton.addEventListener( 'click', function() {
+//   selectedIndex--;
+//   rotateCarousel();
+// });
+
+// var nextButton = document.querySelector('.next-button');
+// nextButton.addEventListener( 'click', function() {
+//   selectedIndex++;
+//   rotateCarousel();
+// });
