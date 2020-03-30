d3dc335a03e86b93788094950cbc84e0fbd7f0de
Gabriel Lopez <lopezg3000@gmail.com>
2020-03-29 21:57:56 -0500

    carouselRenderMethodAdded

--- src/components/roughComponents/animationCarousel.js
+++ src/components/roughComponents/animationCarousel.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { JsxEmit } from 'typescript';
 
 class Carousel extends Component {
   state = {
@@ -6,48 +7,51 @@ class Carousel extends Component {
   }
 
   render() {
-    return (  );
+    return (
+      <React.fragment>
+        <div class="scene">
+          <div class="carousel">
+            <div class="carousel__cell">1</div>
+            <div class="carousel__cell">2</div>
+            <div class="carousel__cell">3</div>
+            <div class="carousel__cell">4</div>
+
+
+          </div>
+        </div>
+        <p style="text-align: center;">
+          <button class="previous-button">Previous</button>
+          <button class="next-button">Next</button>
+        </p>
+      </React.fragment>
+    );
   }
 }
 
 export default Carousel;
 
-<div class="scene">
-  <div class="carousel">
-    <div class="carousel__cell">1</div>
-    <div class="carousel__cell">2</div>
-    <div class="carousel__cell">3</div>
-    <div class="carousel__cell">4</div>
+
+// Js
 
 
-  </div>
-</div>
-  <p style="text-align: center;">
-    <button class="previous-button">Previous</button>
-    <button class="next-button">Next</button>
-  </p>
+// var carousel = document.querySelector('.carousel__cell:nth-child(1)');
+// var cellCount = 4;
+// var selectedIndex = 0;
 
-css
+// function rotateCarousel() {
+//   var angle = selectedIndex / cellCount * -360;
+//   carousel.style.transform = 'translateZ(105px) rotateY(' + angle + 'deg)';
+// }
 
+// var prevButton = document.querySelector('.previous-button');
+// prevButton.addEventListener('click', function () {
+//   selectedIndex--;
+//   rotateCarousel();
+// });
 
+// var nextButton = document.querySelector('.next-button');
+// nextButton.addEventListener('click', function () {
+//   selectedIndex++;
+//   rotateCarousel();
+// });
 
-var carousel = document.querySelector('.carousel__cell:nth-child(1)');
-var cellCount = 4;
-var selectedIndex = 0;
-
-function rotateCarousel() {
-  var angle = selectedIndex / cellCount * -360;
-  carousel.style.transform = 'translateZ(105px) rotateY(' + angle + 'deg)';
-}
-
-var prevButton = document.querySelector('.previous-button');
-prevButton.addEventListener('click', function () {
-  selectedIndex--;
-  rotateCarousel();
-});
-
-var nextButton = document.querySelector('.next-button');
-nextButton.addEventListener('click', function () {
-  selectedIndex++;
-  rotateCarousel();
-});
