ac1ca89fee391676da4191628cb325d3f5d4fcc1
Gabriel Lopez <lopezg3000@gmail.com>
2020-03-29 21:52:09 -0500

    carouselCellsStateAdded

--- src/components/roughComponents/animationCarousel.js
+++ src/components/roughComponents/animationCarousel.js
@@ -1,39 +1,53 @@
-{/* <div class="scene">
+import React, { Component } from 'react';
+
+class Carousel extends Component {
+  state = {
+    carouselCells: ["cellOne", "cellTwo", 'cellThree', 'cellFour']
+  }
+
+  render() {
+    return (  );
+  }
+}
+
+export default Carousel;
+
+<div class="scene">
   <div class="carousel">
     <div class="carousel__cell">1</div>
     <div class="carousel__cell">2</div>
     <div class="carousel__cell">3</div>
     <div class="carousel__cell">4</div>
-    
+
 
   </div>
 </div>
-<p style="text-align: center;">
-  <button class="previous-button">Previous</button>
-  <button class="next-button">Next</button>
-</p> */}
+  <p style="text-align: center;">
+    <button class="previous-button">Previous</button>
+    <button class="next-button">Next</button>
+  </p>
 
-//css
-// 
+css
 
 
-// var carousel = document.querySelector('.carousel__cell:nth-child(1)');
-// var cellCount = 4;
-// var selectedIndex = 0;
 
-// function rotateCarousel() {
-//   var angle = selectedIndex / cellCount * -360;
-//   carousel.style.transform = 'translateZ(105px) rotateY(' + angle + 'deg)';
-// }
+var carousel = document.querySelector('.carousel__cell:nth-child(1)');
+var cellCount = 4;
+var selectedIndex = 0;
 
-// var prevButton = document.querySelector('.previous-button');
-// prevButton.addEventListener( 'click', function() {
-//   selectedIndex--;
-//   rotateCarousel();
-// });
+function rotateCarousel() {
+  var angle = selectedIndex / cellCount * -360;
+  carousel.style.transform = 'translateZ(105px) rotateY(' + angle + 'deg)';
+}
 
-// var nextButton = document.querySelector('.next-button');
-// nextButton.addEventListener( 'click', function() {
-//   selectedIndex++;
-//   rotateCarousel();
-// });
+var prevButton = document.querySelector('.previous-button');
+prevButton.addEventListener('click', function () {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+var nextButton = document.querySelector('.next-button');
+nextButton.addEventListener('click', function () {
+  selectedIndex++;
+  rotateCarousel();
+});
