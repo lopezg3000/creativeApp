32812e242a1bce23c08fbab6203f5f3ed811e726
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-03 08:32:42 -0500

    box shadow added to navbar.css

--- src/homepage/navbar.jsx
+++ src/homepage/navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import './navbar.css';
 
 const NavBar = () => {
     return (
-        <React.Fragment>
+        <nav className='navbar'>
             <Link to='/'><img src='https://skprod.objects.frb.io/images/static/smoothie-king-logo.svg' /></Link>
             <NavLink to='/menu'>Our Menu</NavLink>
             <NavLink to='/ourPromise'>Our Promise</NavLink>
             <NavLink to='/healthyRewards'>Healthy Rewards</NavLink>
             <NavLink to='/locations'>Locations</NavLink>
-            <NavLink to='/franchise'>Franchise Locations</NavLink>
+            <NavLink to='/franchise'>Franchise Information</NavLink>
             <button>Order Now</button>
-        </React.Fragment>
+        </nav>
 
     );
 }
