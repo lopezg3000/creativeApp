f657c5721ba051df38aa1c6d9f4bc10fb80c75d6
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-02 22:22:49 -0500

    navbar tab modules created

--- src/homepage/navbar.jsx
+++ src/homepage/navbar.jsx
@@ -3,7 +3,14 @@ import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <Link><img src='https://skprod.objects.frb.io/images/static/smoothie-king-logo.svg' /></Link>
+        <React.Fragment>
+            <Link><img src='https://skprod.objects.frb.io/images/static/smoothie-king-logo.svg' /></Link>
+            <NavLink>Our Menu</NavLink>
+            <NavLink>Our Promise</NavLink>
+            <NavLink>Healthy Rewards</NavLink>
+            <NavLink>Locations</NavLink>
+            <button>Order Now</button>
+        </React.Fragment>
 
     );
 }
