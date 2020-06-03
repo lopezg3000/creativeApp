4716aa3216d55032af1fcfedee88d7df1669ba42
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-02 21:32:46 -0500

    smoothie king clone responsive header created

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
 import React from 'react';
-import Home from './components/home/home'
+import Header from './homepage/header';
+// import Home from './components/home/home'
 import './App.css';
-import HeroParent from "./components/roughComponents/HeroParent";
-// import Header from "./components/roughComponents/Header"
-// import Information from "./components/roughComponents/Information"
-// import Home from "./components/home/Home"
-// import About from "./components/roughComponents/About"
-// import FavoriteRecipe from "./components/roughComponents/FavoriteRecipe"
-// import SignUpAlert from "./components/roughComponents/SignUpAlert"
-// import HomePage from "./components/roughComponents/HomePage"
-// import SignUpSuccess from './components/roughComponents/SignUpSuccess'
-// import CheckboxParent from './components/CheckBoxParent'
-// import { Route, Switch } from 'react-router-dom'
 // import HeroParent from "./components/roughComponents/HeroParent";
-
 
 
 function App() {
   return (
     <React.Fragment>
-      <HeroParent />
-      <div className="container">
-        <Home />
-      </div>
+      <Header />
     </React.Fragment>
   );
 }
 
 export default App;
 
-  // <React.Fragment>
-  //   <Home />
-  //   {/* <Information /> */}
-  //   {/* <About />
-  //   <Switch> */}
-  //   {/* <Route path='/HomePage' component={HomePage} />
-  //     <Route path='/SignUpSuccess' component={SignUpSuccess} />
-  //   </Switch> */}
-  //   {/* <FavoriteRecipe />
-  //   <SignUpAlert />
-  //   <CheckboxParent /> */}
-  // </React.Fragment>
+{/* <HeroParent />
+  <div className="container">
+    <Home />
+  </div> */}
---
+++ src/homepage/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.css';
+
+
+const Header = () => {
+    return (
+        <header>
+            <p></p>
+            <p>Get in and out fast and </p>
+            <p><strong><u>order online</u></strong></p>
+            <p> or with the </p>
+            <p><strong><u>Healthy Rewards</u></strong></p>
+            <p> app!</p>
+            <p></p>
+        </header>
+    );
+}
+
+export default Header;
