acf7265de1e3f6b7c4ef436204beb87e4b420865
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-10 17:30:55 -0500

    hamburger menu animation implemented by means of checked state

--- src/homepage/navbar.jsx
+++ src/homepage/navbar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
                     <Link to='/'><img src='https://skprod.objects.frb.io/images/static/smoothie-king-logo.svg' /></Link>
                 </div>
                 <div className='menu-wrap'>
-                    <input type='checkbox' class='toggler' />
+                    <input type='checkbox' className='toggler' />
                     <div className='hamburger'><div></div></div>
                     <div className='menu'>
                         <div>
