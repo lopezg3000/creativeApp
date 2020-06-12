3f05b5acd522e91c3b59374782917290f9e6df6d
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-12 13:06:19 -0500

    changed src back to the video src on smoothie king but still don't know if it will load

--- src/homepage/videoBackground.jsx
+++ src/homepage/videoBackground.jsx
@@ -6,7 +6,7 @@ const VideoBackground = () => {
         <div className='hero-background container'>
             <div className='video-wrap'>
                 <video
-                    src='https://www.videvo.net/videvo_files/converted/2013_12/preview/CROWD_JUMPS_LIGHTS_PULSE.mov15296.webm'
+                    src='https://vod-progressive.akamaized.net/exp=1591999507~acl=%2A%2F1454893004.mp4%2A~hmac=1e4d47cfb1faa39d0d901a0ebeaa0a3a238bef32bd7f12cefa43676001e818b4/vimeo-prod-skyfire-std-us/01/1335/14/356679802/1454893004.mp4'
                     autoplay='true'
                     loop='true'
                     type="video/mp4"
