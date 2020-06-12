ad6fb0fbf710d58d1c3b6947debc4653350c398f
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-12 13:03:19 -0500

    bug with video when deployed. changed src

--- src/homepage/videoBackground.jsx
+++ src/homepage/videoBackground.jsx
@@ -6,7 +6,7 @@ const VideoBackground = () => {
         <div className='hero-background container'>
             <div className='video-wrap'>
                 <video
-                    src='https://vod-progressive.akamaized.net/exp=1591982497~acl=%2A%2F1454893004.mp4%2A~hmac=15c6f0323eff33bbf19deb29848d3b682c0e182e518d3137ff4035c8af368329/vimeo-prod-skyfire-std-us/01/1335/14/356679802/1454893004.mp4'
+                    src='https://www.videvo.net/videvo_files/converted/2013_12/preview/CROWD_JUMPS_LIGHTS_PULSE.mov15296.webm'
                     autoplay='true'
                     loop='true'
                     type="video/mp4"
