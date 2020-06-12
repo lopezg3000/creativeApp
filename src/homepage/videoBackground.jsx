4a8564cf0403e07f45881ebd017373fdd9d7a835
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-12 11:41:41 -0500

    styled video but more specifically added bottom property to position video

--- src/homepage/videoBackground.jsx
+++ src/homepage/videoBackground.jsx
@@ -3,11 +3,11 @@ import './videoBackground.css'
 
 const VideoBackground = () => {
     return (
-        <div class='hero-background container'>
-            <div class='video-wrap'>
+        <div className='hero-background container'>
+            <div className='video-wrap'>
                 <video
                     src='https://vod-progressive.akamaized.net/exp=1591982497~acl=%2A%2F1454893004.mp4%2A~hmac=15c6f0323eff33bbf19deb29848d3b682c0e182e518d3137ff4035c8af368329/vimeo-prod-skyfire-std-us/01/1335/14/356679802/1454893004.mp4'
-                    // autoplay='true' 
+                    autoplay='true'
                     loop='true'
                     type="video/mp4"
                     muted>
