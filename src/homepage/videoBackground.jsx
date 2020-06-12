d15a841093692f476cc3756bea7ade9ecaa91027
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-12 17:02:52 -0500

    media query for max width 1023px added and bottom property changed to 2rem for video wrap video selector

--- src/homepage/videoBackground.jsx
+++ src/homepage/videoBackground.jsx
@@ -7,7 +7,7 @@ const VideoBackground = () => {
             <div className='video-wrap'>
                 <video
                     src='https://vod-progressive.akamaized.net/exp=1591999507~acl=%2A%2F1454893004.mp4%2A~hmac=1e4d47cfb1faa39d0d901a0ebeaa0a3a238bef32bd7f12cefa43676001e818b4/vimeo-prod-skyfire-std-us/01/1335/14/356679802/1454893004.mp4'
-                    autoPlay={true}
+                    // autoPlay={true}
                     loop={true}
                     type="video/mp4"
                     muted
