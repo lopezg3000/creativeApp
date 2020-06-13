e1f42dffd7f0da4585db529b53d3564ce87e7a24
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-13 08:18:53 -0500

    hero-copy block position and background added

--- src/homepage/homepage.jsx
+++ src/homepage/homepage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import VideoBackground from './videoBackground'
-import HeroCopyBlock from './heroCopyBlock'
+import VideoBackground from './videoBackground';
+import HeroCopyBlock from './heroCopyBlock';
+import './heroCopyBlock.css'; //need to move this to heroCopyBlock.jsx to be consistent
 
 const HomePage = () => {
     return (
         <React.Fragment>
-            <section>
+            <section className='hero full-width'>
                 <VideoBackground />
                 <HeroCopyBlock />
             </section>
