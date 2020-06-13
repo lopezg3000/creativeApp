84334c951b66b7d390605e60a99ddbbbedb64a14
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-12 21:55:14 -0500

    imported local smoothie video to prevent access denied warning from using url

--- src/homepage/videoBackground.jsx
+++ src/homepage/videoBackground.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SmoothieKing from '../assets/videos/smoothieKing.mp4'
 import './videoBackground.css'
 
 const VideoBackground = () => {
@@ -6,7 +7,7 @@ const VideoBackground = () => {
         <div className='hero-background container'>
             <div className='video-wrap'>
                 <video
-                    src='https://vod-progressive.akamaized.net/exp=1592014411~acl=%2A%2F1454893004.mp4%2A~hmac=9acf1124ca464c5dcaee6737fce646193235934a7d37620e73d4663c2792269b/vimeo-prod-skyfire-std-us/01/1335/14/356679802/1454893004.mp4'
+                    src={SmoothieKing}
                     autoPlay={true}
                     loop={true}
                     type="video/mp4"
