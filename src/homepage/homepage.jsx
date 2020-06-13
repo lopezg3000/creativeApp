f179881f38d0586e79b62c9622ccf53c56d1dc83
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-13 07:14:18 -0500

    copy for heroCopyBlock complete

--- src/homepage/homepage.jsx
+++ src/homepage/homepage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import VideoBackground from './videoBackground'
+import HeroCopyBlock from './heroCopyBlock'
 
 const HomePage = () => {
     return (
         <React.Fragment>
             <section>
                 <VideoBackground />
-
+                <HeroCopyBlock />
             </section>
             <h1>Home</h1>
         </React.Fragment>
