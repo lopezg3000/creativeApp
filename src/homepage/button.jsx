782c6e2ff7e2f2731397fcec7725d7c159447600
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-14 18:29:21 -0500

    button component created

---
+++ src/homepage/button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <a href='#' className='link link-default'>
+            <span>Read More</span>
+            <i class="fas fa-caret-right"></i>
+        </a>
+    );
+}
+
+export default Button;
