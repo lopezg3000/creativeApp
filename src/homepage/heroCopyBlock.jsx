48fd2962e254584c3bc21bfca59f87bd4bfcae61
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-14 07:31:35 -0500

    Photo centered with fixed height and disappearing margin

--- src/homepage/heroCopyBlock.jsx
+++ src/homepage/heroCopyBlock.jsx
@@ -3,22 +3,24 @@ import React from 'react';
 const HeroCopyBlock = () => {
     return (
         <div className='hero-copyblock'>
-            <div className='max-wrapper'>
-                <h2 className='eyebrow'>Let's be part of the change together.</h2>
-                <h3 className='headline large'>Our commitment to help fight racism</h3>
-                <p className='description'>
-                    We stand together as an inclusive brand that’s committed to being a safe
-                    place for all and helping each person live out their purpose. Injustice
-                    and inequality are not right. What is right is using our voice to educate
-                    and our resources to fund causes that fight discrimination.
+            <div className='hero-container'>
+                <div className='max-wrapper'>
+                    <h2 className='eyebrow'>Let's be part of the change together.</h2>
+                    <h3 className='headline large'>Our commitment to help fight racism</h3>
+                    <p className='description'>
+                        We stand together as an inclusive brand that’s committed to being a safe
+                        place for all and helping each person live out their purpose. Injustice
+                        and inequality are not right. What is right is using our voice to educate
+                        and our resources to fund causes that fight discrimination.
                     <br />
-                    <br />
+                        <br />
                     We believe actions speak louder than words. We’re donating $100,000 to
                     the NAACP Legal Defense Fund to promote fair and equitable treatment
                     of all people, and are updating our training to further ensure inclusion
                     and respect in our stores and throughout our organization.
-                </p>
-                <a href='#' className='link link-default'>Read More</a>
+                    </p>
+                    <a href='#' className='link link-default'>Read More</a>
+                </div>
             </div>
         </div>
     );
