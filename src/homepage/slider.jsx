09b1b715bdbfdf9c654b0367c8e54ae8f9b9f0c2
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-17 13:46:55 -0500

    deploy bug image imported a second time

--- src/homepage/slider.jsx
+++ src/homepage/slider.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from './button'
-import ImmuneBuilder from '../assets/img/immuneBuilder.jpg'
+import ImmuneBuilder from '../assets/img/immuneBuilder.jpg';
 import './slider.css';
 
 
