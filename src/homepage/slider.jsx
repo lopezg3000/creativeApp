1a96f35c64b23eebd98238bdafe370a1d246cfb9
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-18 20:01:29 -0500

    the latest goodness slider items complete

--- src/homepage/slider.jsx
+++ src/homepage/slider.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Slide from './slide';
 import ImmuneBuilder from '../assets/img/immuneBuilder.jpg';
+import CleanBlends from '../assets/img/cleanBlends.jpg';
+import FiveDollarFriday from '../assets/img/fiveDollarFriday.jpg';
 import './slider.css';
-{/* <sup><i className="far fa-registered"></i></sup> */ }
 
 class Slider extends Component {
     state = {}
@@ -17,6 +18,29 @@ class Slider extends Component {
         );
     }
 
+    immuneBuilderDescription() {
+        return (
+            <React.Fragment>
+                <span>
+                    Rich in antioxidants, each smoothie is
+                    blended with more than 800% of your daily vitamin C,
+                    plus zinc,iron and calcium to support a healthier
+                    immune system.
+                </span>
+            </React.Fragment>
+        );
+    }
+
+    cleanBlendsDescription() {
+        return (
+            <React.Fragment>
+                <span>It’s our commitment to blend a more nutritious Smoothie.</span>
+                <br />
+                <span>From the bottom of the cup up.</span>
+            </React.Fragment>
+        );
+    }
+
     render() {
         return (
             <div className='slider-container'>
@@ -24,12 +48,23 @@ class Slider extends Component {
                     <Slide
                         eyebrow='New flavor. Same benefits.'
                         headline={this.immuneBuilderHeadline()}
-                        description='Rich in antioxidants, each smoothie is 
-                            blended with more than 800% of your daily vitamin C, 
-                            plus zinc,iron and calcium to support a healthier 
-                            immune system.'
+                        description={this.immuneBuilderDescription()}
                         src={ImmuneBuilder}
                         text='Show me more'
+                    />
+                    <Slide
+                        eyebrow='Our Promise'
+                        headline='Clean Blends'
+                        description={this.cleanBlendsDescription()}
+                        src={CleanBlends}
+                        text='Get the Clean Blends Detail'
+                    />
+                    <Slide
+                        eyebrow='Is it Friday?'
+                        headline='Get Yours For Just $5'
+                        description='Every Friday, any regular 32 oz. Smoothie is just $5.'
+                        src={FiveDollarFriday}
+                        text='How do you Friday?'
                     />
                 </div>
             </div>
