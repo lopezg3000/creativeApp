db9e72f0e530759e51b0e6c3079eb4ca0f485713
Gabriel Lopez <lopezg3000@gmail.com>
2020-06-26 00:55:31 -0500

    transition added to button to smooth hover state when changing color to black

--- src/homepage/sliderNav.jsx
+++ src/homepage/sliderNav.jsx
@@ -5,13 +5,13 @@ const SliderNav = ({ sliderItems, activeSlide, onPrevClick, onNextClick }) => {
     return (
         <div className='slider-nav'>
             <button onClick={onPrevClick}>
-                <i className="far fa-arrow-alt-circle-left icon"></i>
+                <i className="fas fa-chevron-left icon"></i>
             </button>
             <span className='slider-nav-current'>{activeSlide}</span>
             &nbsp;/&nbsp;
             <span className='slider-nav-total'>{sliderItems}</span>
             <button onClick={onNextClick}>
-                <i className="far fa-arrow-alt-circle-right icon"></i>
+                <i className="fas fa-chevron-right icon"></i>
             </button>
         </div>
     );
