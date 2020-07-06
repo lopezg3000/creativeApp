4102fba5ac8d31e16fbd78fd9eaafe55da0a0a2d
Gabriel Lopez <lopezg3000@gmail.com>
2020-07-05 21:06:16 -0500

    commented slider ref and any other code associated width resize event listener

--- src/homepage/slider.jsx
+++ src/homepage/slider.jsx
@@ -16,25 +16,25 @@ import './slider.css';
 
 class Slider extends Component {
     state = {
-        width: 0,
+        width: 375,
         activeIndex: 0,
         xInitial: '',
         sliderItems: 3
     }
 
-    componentDidMount() {
-        this.updateDimensions();
-        window.addEventListener('resize', this.updateDimensions);
-    };
+    // componentDidMount() {
+    //     this.updateDimensions();
+    //     window.addEventListener('resize', this.updateDimensions);
+    // };
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateDimensions);
-    };
+    // componentWillUnmount() {
+    //     window.removeEventListener('resize', this.updateDimensions);
+    // };
 
-    updateDimensions = () => {
-        const width = this.slider.clientWidth;
-        this.setState({ width })
-    };
+    // updateDimensions = () => {
+    //     const width = this.slider.clientWidth;
+    //     this.setState({ width })
+    // };
 
     immuneBuilderHeadline() {
         return (
@@ -143,7 +143,7 @@ class Slider extends Component {
             <React.Fragment>
                 <div
                     className='slider-container'
-                    ref={(slider) => { this.slider = slider }}
+                    // ref={(slider) => { this.slider = slider }}
                     onTouchStart={this.handleTouchStart}
                     onMouseUp={this.handleMouseUp}
                     onMouseDown={this.handleMouseDown}
