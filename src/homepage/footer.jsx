811a67503e7ff95b7099e21dfd84609ec13e1d4f
Gabriel Lopez <lopezg3000@gmail.com>
2020-07-17 22:50:39 -0500

    html structure for column left 75% complete but still need to add social-follow-buttons

--- src/homepage/footer.jsx
+++ src/homepage/footer.jsx
@@ -7,6 +7,32 @@ const Footer = () => {
             <div className='max-wrapper'>
                 <div className='column column-left'>
                     <Link to='/'><img src='https://skprod.objects.frb.io/images/static/smoothie-king-logo.svg' /></Link>
+                    <div className='mailing-list-signup'>
+                        <p>
+                            Be the first to learn of new products and
+                            promotions when you sign up for our newsletter.
+                        </p>
+                        <form>
+                            <div className='entry-email-signup'>
+                                <input
+                                    name='s_email'
+                                    type='text'
+                                    placeholder='yourname@domain.com'
+                                    className='entry-input-text'
+                                />
+                                <div className='checkbox'>
+                                    <input className='confirm' type='checkbox' />
+                                    <label for='confirm'>
+                                        <p>I agree to receive emails from Smoothie King.</p>
+                                    </label>
+                                </div>
+                                <button type='submit' className='button-submit'>
+                                    <i className="fas fa-caret-right icon white"></i>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className='social-follow-buttons'></div>
                 </div>
                 <div className='column column-center'></div>
                 <div className='column column-right'></div>
