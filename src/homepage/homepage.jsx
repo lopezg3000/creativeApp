3a31386be88df40cb05e30b2db78b2b1a52ef32d
Gabriel Lopez <lopezg3000@gmail.com>
2020-07-28 11:39:29 -0500

    refs created for each section and targets observed respectively

--- src/homepage/homepage.jsx
+++ src/homepage/homepage.jsx
@@ -20,61 +20,97 @@ import { entries } from 'lodash';
 class Homepage extends Component {
     //Used createRef method instead of ref callback because ref created by createRef
     //method happens before componentDidMount
-    heroRef = React.createRef();
-
     state = {
-        isVisible: false
+        isVisible: false,
     }
 
+    sections = [
+        { number: 1 },
+        { number: 2 },
+        { number: 3 },
+        { number: 4 },
+        { number: 5 },
+        { number: 6 },
+        { number: 7 }
+    ]
+
+    heroRef = React.createRef();
+    sectionRefs = this.sections.reduce((acc, value) => {
+        acc[value.number] = React.createRef();
+        return acc;
+    }, {});
+
+
     componentDidMount() {
+
+        // console.log(this.sectionRefs);
+
         // console.log('hero');
         this.observer = new IntersectionObserver(
             this.handleObserver.bind(this)
         );
 
-        this.observer.observe(this.heroRef.current);
+        // if (this.heroRef.current) {
+        Object.values(this.sectionRefs).forEach(value => {
+            // console.log(value.current)
+            if (value.current) this.observer.observe(value.current)
+        },
+        );
+        // this.observer.observe(this.heroRef.current);
+        // console.log(this.heroRef.current);
+        // }
+
     }
 
     componentWillUnmount() {
-        this.observer.unobserve(this.heroRef.current);
+        Object.values(this.sectionRefs).forEach(value => {
+            if (value.current) this.observer.unobserve(value.current)
+        });
+        // this.observer.unobserve(this.heroRef.current);
     }
 
-    handleObserver = (entries, observer) => {
-        console.log('hello');
+    handleObserver(entries, observer) {
         if (entries[0].isIntersecting) {
+            console.log(entries.target)
             this.setState({ isVisible: true });
 
-            observer.unobserve(this.heroRef.current);
+            Object.values(this.sectionRefs).forEach(value => {
+                if (value.current) observer.unobserve(value.current)
+            }
+            );
+            // observer.unobserve(this.heroRef.current);
         }
-
-        // if (this.heroRef.current) {
-        //     observer.observe(this.heroRef.current)
-        // }
     }
 
 
     render() {
+
+        // console.log(this.sectionRefs[1]);
+        // console.log(this.heroRef);
         return (
             <React.Fragment>
                 <section
                     className={`hero full-width section ${this.state.isVisible ? 'is-visible' : ''}`}
-                    // ref={heroRef => (this.heroRef = heroRef)}
-                    ref={this.heroRef}
+                    // ref={this.heroRef}
+                    ref={this.sectionRefs[1]}
                 >
                     <VideoBackground />
                     <HeroCopyBlock />
                 </section>
-                <section className='content-slider carousel'>
+                <section
+                    className={`content-slider carousel section ${this.state.isVisible ? 'is-visible' : ''}`}
+                    ref={this.sectionRefs[2]}
+                >
                     <ContentIntro
                         headline='The Latest Smoothie Goodness'
                         description='Keep tabs on new products, partnerships, and limited-time offers.'
                     />
                     <Slider />
                 </section>
-                <section className='full-width-promo'>
+                <section className='full-width-promo' ref={this.sectionRefs[3]}>
                     <Promo />
                 </section>
-                <section className='smoothie-categories stacked'>
+                <section className='smoothie-categories stacked' ref={this.sectionRefs[4]}>
                     <ContentIntro
                         eyebrow='Purpose Blends'
                         headline='Blended to Fuel Your Passion'
@@ -82,13 +118,13 @@ class Homepage extends Component {
                     />
                     <SmoothieCategories />
                 </section>
-                <section className='content-slider partnerships carousel'>
+                <section className='content-slider partnerships carousel' ref={this.sectionRefs[5]}>
                     <PartnershipsSlider />
                 </section>
-                <section className='promo-unit left'>
+                <section className='promo-unit left' ref={this.sectionRefs[6]}>
                     <PromoUnit />
                 </section>
-                <section className='location-block'>
+                <section className='location-block' ref={this.sectionRefs[7]}>
                     <LocationBlock />
                 </section>
             </React.Fragment>
